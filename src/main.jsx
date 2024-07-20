8da666649eec3cda7d9c734f7ccc61aa3659d7ae
Adeola-creator <111973534+deeygitalstudio@users.noreply.github.com>
2024-07-20 19:19:34 +0100

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import {BrowserRouter} from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+   <BrowserRouter basename="/patrol-tracker">
   
   
       <App />
