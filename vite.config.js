482db30047bb921a72d256038a94564b69529b54
Adeola-creator <111973534+deeygitalstudio@users.noreply.github.com>
2024-07-22 00:38:10 +0100

    Add 404.html for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/patrol-tracker/', // Set this to the name of your repo
+  base: '/patrol-tracker/', // Make sure this matches your repository name
   build: {
     rollupOptions: {
       output: {
         manualChunks: undefined,
       },
     },
-  },
-  server: {
-    historyApiFallback: true,
   },
 });
