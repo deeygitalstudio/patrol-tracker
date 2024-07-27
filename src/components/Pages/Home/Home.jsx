add139a114928a51caf43af372cc2c700b803f9a
Adeola-creator <111973534+deeygitalstudio@users.noreply.github.com>
2024-07-27 07:20:36 +0100

    css

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = ({setShowLogin}) => {
     <div className="col-md-5  --flex-dir-column">
       <h1 className='--fw-bold title'>Document your patrol with ease</h1>
       <p className=''>This form is to capture the patrol of zonal assistants especially where they discharge their duties of visiting locations and maintaining Halogen standards</p>
-  <button onClick={() => setShowLogin(true)} className="btn  btt" style={{ backgroundColor: '#17265D', color: '#fff' }}>Get Started</button>
+  <button onClick={() => setShowLogin(true)} className="--btn  btt" style={{ backgroundColor: '#17265D', color: '#fff' }}>Get Started</button>
     </div>
     <div className="col-md-7 --align-center">
       <img src={image} alt="" />
@@ -21,7 +21,7 @@ const Home = ({setShowLogin}) => {
     
     
     </div>
-    <button onClick={() => setShowLogin(true)} className="btn  bts" style={{ backgroundColor: '#17265D', color: '#fff' }}>Get Started</button>  
+    <button onClick={() => setShowLogin(true)} className="--btn  bts" style={{ backgroundColor: '#17265D', color: '#fff' }}>Get Started</button>  
      
     </div>
     </section>
